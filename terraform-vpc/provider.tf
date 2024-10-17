d1d1fb0a7974c769e31edc0c2b14deb2744f6f70
Hariprasath <hariguru798@gmail.com>
2024-10-18 00:24:19 +0530

    added Ec2 instances

--- terraform-vpc/provider.tf
+++ terraform-vpc/provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
     region = "us-east-1"
-    access_key = "AKIAU72LGE6TW5WJE7E3"
-    secret_key = "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"
+    access_key = ""
+    secret_key = ""
 }
